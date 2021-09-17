124d38596fad10def73c38cac305eb9a03009871
josecarlos20 <78504539+josecarlos20@users.noreply.github.com>
2021-09-17 17:59:58 -0500

    Add files via upload

---
+++ Class4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace proyecto
+{
+	class item
+	{
+		
+		//atributo
+		private string nombre;
+		private double durabilidad;
+		private int nivel;
+
+
+		//metodo
+		public void usarObjeto()
+		{
+
+		}
+
+
+	}
+}
