71f6424a092a0cc1a26e645735b72ea54e816053
JhonatanMarinS <81063802+JhonatanMarinS@users.noreply.github.com>
2021-09-17 18:09:28 -0500

    Clases
    Clases Personaje, Jugador, Enemigo, Boss y Criatura

---
+++ Clases.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto1
+{
+    class Master
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+    class Personaje
+    {
+        //Atributos
+        private String nombre;
+        private char sexo;
+        private double ataque;
+        private double vida;
+        private double resistencia;
+        private int nivel;
+
+        //Metodos
+
+        public void golpear()
+        {
+
+        }
+        public void recibirImpacto()
+        {
+
+        }
+        public void moverArriba()
+        {
+
+        }
+        public void moverIzquierda()
+        {
+
+        }
+        public void moverAbajo()
+        {
+
+        }
+        public void moverDerecha()
+        {
+
+        }
+        public double calcularResistencia()
+        {
+
+        }
+        public double calcularAtaque()
+        {
+
+        }
+    }
+    class Jugador
+    {
+        //Atributos
+
+        //Metodos
+        public void golpear()
+        {
+
+        }
+        public void consumir()
+        {
+
+        }
+    }
+
+    class Enemigo
+    {
+        //Atributos
+        private String tipo;
+
+        //Metodos
+        public void golpear()
+        {
+
+        }
+
+        public void localizacion()
+        {
+
+        }
+
+        public void recibirImpacto()
+        {
+
+        }
+
+        public void atacar()
+        {
+
+        }
+    }
+    class Boss
+    {
+        //Atributos
+        private int evolucionesAplicadas;
+
+        //Metodos
+        public void evolucionar()
+        {
+
+        }
+    }
+
+    class criatura
+    {
+        //Atributos
+        private String especie;
+        private double radioAtaque;
+
+        //Metodos
+        public void RadioAccion()
+        {
+
+        }
+
+    }
+
+
+}
